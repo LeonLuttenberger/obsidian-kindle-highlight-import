570f7917c3fb5fa6d01f7a6804be26f21b727dcf
Leon Luttenberger <LeonLuttenberger@users.noreply.github.com>
2025-05-04 11:07:36 -0600

    fix: Make file upload modal invisible, apart from the file picker itself (#42)

--- main.ts
+++ main.ts
@@ -10,15 +10,6 @@ export default class KindleImportPlugin extends Plugin {
   async onload() {
     await this.loadSettings();
 
-    // This adds a simple command that can be triggered anywhere
-    this.addCommand({
-      id: "kindle-import",
-      name: "Import Kindle Notebook from Vault",
-      callback: () => {
-        new KindleSelectionModal(this.app, this.settings).open();
-      },
-    });
-
     this.addCommand({
       id: "kindle-import-from-file-picker",
       name: "Import Kindle Notebook with File Picker",
@@ -27,14 +18,16 @@ export default class KindleImportPlugin extends Plugin {
       },
     });
 
+    this.addCommand({
+      id: "kindle-import",
+      name: "Import Kindle Notebook from Vault",
+      callback: () => {
+        new KindleSelectionModal(this.app, this.settings).open();
+      },
+    });
+
     // This adds a settings tab so the user can configure various aspects of the plugin
     this.addSettingTab(new SampleSettingTab(this.app, this));
-
-    // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-    // Using this function will automatically remove the event listener when this plugin is disabled.
-    this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-      console.log("click", evt);
-    });
 
     // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
     this.registerInterval(window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000));
